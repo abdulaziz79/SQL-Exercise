aa0cabdd5c390e758bd33fd899ea0caf5f3cf578
abdulaziz <aboudecharkawi@gmail.com>
2023-09-18 16:50:26 +0300

    basic queries done

---
+++ answers.sh
@@ -0,0 +1,18 @@
+"SELECT name FROM students"
+
+"SELECT * FROM students WHERE Age > 30"
+
+"SELECT name FROM students WHERE Gender = "F" AND Age=30"
+
+"SELECT Points FROM students where name ="Alex""
+
+"INSERT INTO students (Name, Age, Gender, Points)
+VALUES ("Abdulaziz", 24, "M", 500)"
+
+"UPDATE students
+SET Points=400
+WHERE name="Basma""
+
+"UPDATE students
+SET Points=150
+WHERE name="Alex""
