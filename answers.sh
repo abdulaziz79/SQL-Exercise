05ff3977762bdf2c948464152a7fdbb68e246f1c
abdulaziz <aboudecharkawi@gmail.com>
2023-09-19 00:42:26 +0300

    Joins

--- answers.sh
+++ answers.sh
@@ -1,3 +1,5 @@
+1-
+
 "SELECT name FROM students"
 
 "SELECT * FROM students WHERE Age > 30"
@@ -40,3 +42,17 @@ WHERE name="Layal"
 
 DELETE FROM students
 WHERE name= "Layal";
+
+3- 
+
+"SELECT employees.Name,companies.Name, companies.Date FROM companies
+INNER JOIN employees"
+
+"SELECT employees.name FROM companies
+INNER JOIN employees
+on companies.Name = employees.Company
+WHERE companies.Date < 2000"
+
+"SELECT companies.name FROM companies
+INNER JOIN employees on employees.Company=companies.Name
+WHERE employees.Role="graphic designer"""
